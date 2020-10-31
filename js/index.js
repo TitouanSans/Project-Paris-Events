4ea7206e5289665f41df1e6209e1df0d6ca2274d
Titouan SANS <Titouan.sanshochart@gmail.com>
2020-10-31 15:49:23 +0100

    complete search

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 
 const favStorage = localStorage;
 
+$('#submit').on('click', function (e) {
+    e.preventDefault();
+    let title = $('#title').val();
+    let url = `https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?search=${title}`
+
+    console.log(url);
+    onSubmit(url);
+});
+
 jQuery.ajax({
     url : 'https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records', // La ressource ciblée
     dataType : 'jsonp' // Le type de la requête HTTP.
@@ -21,5 +30,34 @@ jQuery.ajax({
         `);
 
     };
+
+    
 });
 
+function onSubmit(url) {
+    $('#events').empty();
+
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+    }).then((result) => {
+
+        const results = result.records;
+        console.log(results);
+        console.log(results.length);
+
+        for (let i = 0; i < results.length; i++) {
+            $('#events').append(`
+                <article>
+                    <img src="${results[i].record.fields.cover_url}" class="eventsCov"></img>
+                    <h2>${results[i].record.fields.title}</h2>
+                    <p>${results[i].record.fields.date_description}</p>
+                    <p>${results[i].record.fields.lead_text}</p>
+                    <a><button class="btnFav">Favoris</button></a>
+                </article>
+            `);
+    
+        };
+
+    })
+}
